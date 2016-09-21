bc90b5641f7bcc10320435dabd641559cff2ac6a
Jordan DeHerrera <deherrera@uber.com>
2016-09-21 17:02:14 -0500

    Added ability to pick range of weeks to optimize

--- survivor.R
+++ survivor.R
@@ -8,6 +8,9 @@ library(Rglpk)
 
 final <- read.csv(file="schedule.csv",row.names=NULL, stringsAsFactors=F)
 
+# Only pick from weeks 3 through 7
+final <- final[final$Week %in% 3:8,-1]
+
 teams <- sort(unique(final$Team))
 
 # number of variables
@@ -17,31 +20,38 @@ obj <- final$ELODiff
 # the vars are represented as booleans
 var.types <- rep("B", num.picks)
 # the constraints
-matrix <- rbind(diag(final$TeamELO),
-                diag(final$ELODiff)
-                )
+matrix <- rbind(#diag(final$TeamELO),
+                 diag(final$ELODiff)
+                 )
+
+# Create empty matrix
+#matrix <- matrix(,nrow=0, ncol=num.picks)
 
 for (i in 1:length(teams)){
   newrow <- as.numeric(final$Team == teams[i])
   matrix <- rbind(matrix,newrow)
 }
 
-for (i in 1:17){
+for (i in min(unique(final$Week)):max(unique(final$Week))){
   newrow <- as.numeric(final$Week == i)
   matrix <- rbind(matrix,newrow)
 }
 
 matrix <- as.matrix(matrix)
 
-direction <- c(rep(">=", num.picks),
+rownames(matrix) <- 1:nrow(matrix)
+
+colnames(matrix) <- 1:ncol(matrix)
+
+direction <- c(#rep(">=", num.picks),
                rep(">=", num.picks),
                rep("<=", length(teams)),
-               rep("==", 17)
+               rep("==", length(unique(final$Week)))
               )
-rhs <- c(rep(0, num.picks),
-         rep(0, num.picks),
+rhs <- c(#rep(0, num.picks), Minimum ELO for picked team
+         rep(0, num.picks), #Minimum ELO point spread
          rep(1, length(teams)),
-         rep(1, 17))
+         rep(1, length(unique(final$Week))))
 
 sol <- Rglpk_solve_LP(obj = obj, mat = matrix, dir = direction, rhs = rhs,
                       types = var.types, max = TRUE)
@@ -51,3 +61,4 @@ finalSelection <- final[sol$solution == 1,]
 finalSelection <- finalSelection[order(finalSelection$Week),]
 
 write.csv(finalSelection,file="SurvivorPicks.csv")
+
