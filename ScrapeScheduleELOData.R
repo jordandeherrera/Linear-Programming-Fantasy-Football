c733f323d9fa373f8698db03417102abf9b1b7b6
Jordan DeHerrera <deherrera@uber.com>
2016-09-26 10:11:57 -0500

    Added weekly updating functionality with regular expressions

--- ScrapeScheduleELOData.R
+++ ScrapeScheduleELOData.R
@@ -1,40 +1,73 @@
-library(htmltab)
-library(tidyr)
-library(reshape)
+## Regular Expressions Examples
+
+## Uses default regex function as well as some stringr functions
+## browseVignettes(package = "stringr")
+
+## Tools for understanding and getting started with Regular Expressions
+# Reference sheet:  http://www.regular-expressions.info/reference.html
+# Interactively test regular expressions:  http://gskinner.com/RegExr/
+# Tool to build regular expressions for you:  http://www.txt2re.com/
+
+##############################
+##
+## LOAD PACKAGES
+##
+##############################
+
+## Packages
+
+packages <- c(
+  "stringr"
+  ,"dplyr"
+  ,"htmltab"
+  ,"tidyr"
+  ,"reshape"
+)
+
+.loadPackages <- function(p) {require(p, character.only=TRUE, lib.loc = .libPaths())}
+
+lapply(packages, .loadPackages)
+
 
 ############################
 ##
-## SAMPLE 
+## GET AND CLEAN DATA
 ##
 ############################
-
-url <- "http://en.wikipedia.org/wiki/Demography_of_the_United_Kingdom"
-ukRel <- htmltab(doc = url, which = "//th[text() = 'Religion']/ancestor::table")
-
-# Clean Table
-ukRel <- gather(ukRel, key, value, -Religion)
-ukRel <- separate(ukRel, key, into = c("year", "statistic"), sep = " >> ")
 
 url <- "http://projects.fivethirtyeight.com/2016-nfl-predictions/"
-xp2 <- "//td[text() = '1643']/ancestor::table"
+xp2 <- "//td[text() = 'AFC West']/ancestor::table"
 fbELO <- htmltab(doc = url, which = xp2, header = 0, encoding = "UTF-8", colNames = c("elo", "change","team","div","wins","losses",
                                                                                       "pointdiff","playoffs","windiv","bye","sb"))
 fbELO <- fbELO[,c(1,3)]
 colnames(fbELO) <- c("elo","team")
-#fbELO$team <- gsub("0-1","",fbELO$team)
-#fbELO$team <- gsub("1-0","",fbELO$team)
-#fbELO$team <- gsub("1-1","",fbELO$team)
-#fbELO$team <- gsub("0-2","",fbELO$team)
-#fbELO$team <- gsub("2-0","",fbELO$team)
 
-teamAbb <- data.frame(Long = fbELO$team, Abb = c("DEN","CAR","SEA","PIT","NE",
-                                                 "KC","ARI","GB","MIN","CIN",
-                                                 "HOU","NYJ","DET","BAL","PHI","NYG",
-                                                 "BUF","IND","OAK","DAL","SF","ATL",
-                                                 "LA","WSH","NO","TB","SD","CHI","MIA",
-                                                 "JAX","CLE","TEN"))
+## Regular Expression to Clean Team Names and Separate Name from Record
 
-fbELO$team <- teamAbb$Abb
+# Regular Expression for Use in Text Extraction
+# Locate team record and take all characters other than
+# Record
+regex = "[0-9]+[-.][0-9]+"
+
+# Replace existing teams with records with just the team name
+fbELO$team <- unlist(strsplit(fbELO$team, regex))
+
+## Match and replace names with abbreviations
+# Read in city data
+cities <- read.csv("TeamAbbreviations.csv",stringsAsFactors = F)
+
+# Match and replace
+fbELO <- inner_join(fbELO,cities,by=c("team" = "City"))
+fbELO$team <- fbELO$Abbreviation
+
+# Eliminate extraneous columns
+fbELO <- select(fbELO, elo,team)
+
+############################
+##
+## SCHEDULE DATA
+##
+############################
 
 url <- "http://www.espn.com/nfl/schedulegrid"
 xp2 <- "//td[text() = 'TEAM']/ancestor::table"
@@ -52,7 +85,8 @@ colnames(fbSchedule) <- c("Week","Team","Opponent")
 # Prepare data for matching
 fbSchedule$Team <- as.character(fbSchedule$Team)
 fbSchedule$Opponent <- as.character(fbSchedule$Opponent)
-fbELO$Team <- as.character(fbELO$team)
+
+fbELO$Team <- fbELO$team
 fbELO <- fbELO[,-2]
 fbELO <- rbind(fbELO, data.frame(elo=0,Team="BYE"))
 
